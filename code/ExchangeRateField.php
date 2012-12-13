87dee01ae3f1581508676efe3e0523166b77fa1a
Frank Mullenger <frankmullenger@gmail.com>
2012-12-13 14:31:46 +1300

    NEW: Included javascript to auto submit form when currency field changes.

--- code/ExchangeRateField.php
+++ code/ExchangeRateField.php
@@ -5,6 +5,7 @@ class ExchangeRateField extends DropdownField {
 	function FieldHolder($properties = array()) {
 
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
 		Requirements::javascript('swipestripe-currency/javascript/ExchangeRateField.js');
 
 		return parent::FieldHolder();
--- javascript/ExchangeRateField.js
+++ javascript/ExchangeRateField.js
@@ -1,4 +1,23 @@
-(function($) { 
-    $(document).ready(function() { 
-    })
+;(function($) { 
+	$.entwine('sws', function($){
+
+		$('select.exchangerate').entwine({
+
+			onmatch : function() {
+				var self = this;
+				var form = this.closest('form');
+
+				this.on('change', function(e){
+					form.submit();
+				});
+
+				this._super();
+			},
+
+			onunmatch: function() {
+				this._super();
+			},
+		});
+
+	});
 })(jQuery);
